abb53f206c199638f4ab4e1b7ce4a46870744f33
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-04-28 19:14:42 +0530

    Update checkAWSkeys2.java

--- Test/checkAWSkeys2.java
+++ Test/checkAWSkeys2.java
@@ -2,6 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     }  
-    aws_access_key_id = AKIAIOSFODNN7EXAMPLE
+    test_variable = AKIAIOSFODNN7EXAMPLE
     aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 } 
