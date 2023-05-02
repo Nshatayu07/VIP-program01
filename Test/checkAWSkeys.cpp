2158e3c3cd4bf45845e552406c22763b7f3f99f8
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-05-02 14:37:16 +0530

    Create checkAWSkeys.cpp
    Checking for access key detection in Cpp

---
+++ Test/checkAWSkeys.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+  
+// Driver code
+int main()
+{
+  string name;
+      
+  cout << "Enter the name: ";
+  cin >> name;
+    
+  cout << "Entered name is: " << 
+           name; 
+  string my_key = "AKIAIOSFODNN7EXAMPLE";
+  string my_key_2 = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+  
+  return 0;
+}
