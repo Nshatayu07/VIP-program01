4bc7289c6d07053578c49fba7bc8533a312abe14
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-05-02 15:29:37 +0530

    Create checkAWSkeys.js
    Checking for access key detection in javaScript

---
+++ Test/checkAWSkeys.js
@@ -0,0 +1,8 @@
+ function func1(){}
+    func1.displayName="someName"
+    console.log(func1.displayName)
+
+test_var =  "AKIAIOSFODNN7EXAMPLE"
+target_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+console.log(test_var);
