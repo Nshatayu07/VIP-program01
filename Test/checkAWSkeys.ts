b8156061d30d011c3cb9f9217a4c73d90a50b2d2
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-05-02 15:47:02 +0530

    Create checkAWSkeys.ts
    Checking for access key detection in typeScript

---
+++ Test/checkAWSkeys.ts
@@ -0,0 +1,8 @@
+function addNumbers(a, b) {  
+    return a + b;  
+}  
+var sum = addNumbers("JavaTpoint", 25);  
+console.log('Sum of the numbers is: ' + sum); 
+
+var access_key = "AKIAIOSFODNN7EXAMPLE";
+var aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY;
