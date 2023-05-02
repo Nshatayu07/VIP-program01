77fb47e41b2aceddccc1e0d006c3b872e140ed46
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-05-02 14:29:45 +0530

    Create checkAWSkeys.py
    adding access key to python file

---
+++ Test/checkAWSkeys.py
@@ -0,0 +1,8 @@
+def personal_details():
+    name, age = "Simon", 19
+    address = "Bangalore, Karnataka, India"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+    personal_id = "AKIAIOSFODNN7EXAMPLE",
+    mapping_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+personal_details()
